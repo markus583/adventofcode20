681a31ed6348976ec64d2ed5258e748de0c2ea86
markus583 <markus.frohmann@gmail.com>
2020-12-12 21:01:30 +0100

    123

--- 1.py
+++ 1.py
@@ -1,14 +1,15 @@
 from  itertools import combinations
 
-with open('data/1.txt', newline='') as f:
+with open('data/1.txt', 'r') as f:
     text = f.read()
 
-data = text.split('\r\n')
+
+data = text.split('\n')
 all_feature_pairs = list(combinations(data, 2))
 correct_answer = []
 for (one, two) in all_feature_pairs:
-    one_proc = int(one[0])
-    two_proc = int(two[0])
+    one_proc = int(one)
+    two_proc = int(two)
     if one_proc + two_proc == 2020:
         correct_answer = [one_proc, two_proc]
         break
@@ -17,13 +18,12 @@ for (one, two) in all_feature_pairs:
 print(f'{one_proc} + {two_proc} = 2020, which was our goal.\nFinally, {one_proc} * {two_proc} = {one_proc * two_proc}!')
 
 
-data[0][0] = '2008'
 all_feature_pairs = list(combinations(data, 3))
 correct_answer = []
 for (one, two, three) in all_feature_pairs:
-    one_proc = int(one[0])
-    two_proc = int(two[0])
-    three_proc = int(three[0])
+    one_proc = int(one)
+    two_proc = int(two)
+    three_proc = int(three)
 
     if one_proc + two_proc + three_proc == 2020:
         correct_answer = [one_proc, two_proc, three_proc]
