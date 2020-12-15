18cb21c44bfdb38f7e6996d7d3bb02ce2952748a
markus583 <markus.frohmann@gmail.com>
2020-12-15 11:39:59 +0100

    added day 15

--- 15.py
+++ 15.py
@@ -1,26 +1,34 @@
-with open('data/15.txt', 'r') as f:
-    text = f.read()
-
-input = [int(number) for number in text.split(',')]
-my_dict = dict()
-
-for player, number in enumerate(range(11)):
-    if player < len(input):
-        my_dict[number] = input[player]
-        continue
-    current_dict = {v: [k for k in my_dict if my_dict[k] == v] for v in set(my_dict.values())}
-    for index, num_player in enumerate(current_dict.values()):
+def get_number_spoken(input, timestamp):
+    # process input
+    input = [int(number) for number in input.split(',')]
+    my_dict = dict()
+    # loop over all players until timestamp
+    for player, number in enumerate(range(timestamp)):
         success = False
-        if index + 1 == len(num_player):
-            my_dict[number] = 0
-            break
-        for current_number in num_player:
-            if current_number in current_dict.keys():
-                if len(num_player) > 1 and my_dict[player - 1] == current_dict[current_number]:
+        # in case of first round
+        if player < len(input):
+            my_dict[number] = input[player]
+            continue
+        # find double values
+        current_dict = {v: [k for k in my_dict if my_dict[k] == v] for v in set(my_dict.values())}
+        keys = list(current_dict.keys())
+        values = list(current_dict.values())
+        for index, num_player in enumerate(current_dict.values()):
+            if success:
+                break
+            success = False
+            for current_number in num_player:
+                if len(num_player) > 1 and my_dict[player - 1] == keys[values.index(num_player)]:
                     my_dict[number] = num_player[-1] - num_player[-2]
                     success = True
                     break
+                elif 1 == len(num_player):
+                    my_dict[number] = 0
+    return my_dict[timestamp-1]
 
 
-# my_dict[7] = 19
-print(my_dict)
+if __name__ == '__main__':
+    with open('data/15.txt', 'r') as f:
+        text = f.read()
+    result = get_number_spoken(text, 2020)
+    print(result)
